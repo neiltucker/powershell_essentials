e910030440259a579013c928405a5ba80718fd74
Neil Tucker <49943805+neiltucker@users.noreply.github.com>
2023-05-10 07:44:40 -0400

    Update Exercise1.ps1

--- Exercise1.ps1
+++ Exercise1.ps1
@@ -1,22 +1,33 @@
 #  PowerShell Essentials: Exercise 1
 
 Get-Command
+gcm
 Get-Help | More
-Get-Help Get-WmiObject -Detail | More
+Get-Help Get-CimInstance -Detail | More
 Get-Help *about*
 Get-Help About_Scripts | More
-
-CD ~; Cls; Dir
+New-Item –Path c:\labfiles –type directory -erroraction SilentlyContinue
 Get-Alias CD; Get-Alias Cls; Get-Alias Dir
-Get-WmiObject Win32_NetworkAdapterConfiguration
-Get-WmiObject -Query "Select * From Win32_NetworkAdapterConfiguration"
-Get-WmiObject -Query "Select * From Win32_NetworkAdapterConfiguration Where DNSDomain='Contoso.com'"
-Get-WmiObject -Query "Select * From Win32_NetworkAdapterConfiguration" | Format-Table IPAddress,MacAddress 
-Get-WmiObject Win32_ComputerSystem | Format-Table Name,Domain,TotalPhysicalMemory
-Get-WmiObject Win32_ComputerSystem | Format-Table Name,Domain,TotalPhysicalMemory
-Set-Location HKLM:\Software; Dir; Set-Location C:
+cd C:\Labfiles
+Set-Location C:\Labfiles
+cls
+Clear-Host
+dir
+Get-ChildItem
+Set-Location C:\Labfiles; Clear-Host; Get-ChildItem
+Get-CimInstance Win32_NetworkAdapterConfiguration
+Get-CimInstance -Query "Select * From Win32_NetworkAdapterConfiguration"
+Get-CimInstance -Query "Select * From Win32_NetworkAdapterConfiguration Where DNSDomain='Contoso.com'"
+Get-CimInstance -Query "Select * From Win32_NetworkAdapterConfiguration" | Format-Table IPAddress,MacAddress 
+Get-CimInstance Win32_ComputerSystem | Format-Table Name,Domain,TotalPhysicalMemory
+Set-Location HKLM:\Software; Get-ChildItem; 
+Set-Location C:
 New-Item –Path c:\temp\tmp –type directory
-New-Item –Path c:\temp\tmp\test.txt –type file
-Set-Alias Copy-Con Set-Content
-Copy-Con test.txt “This is a test”
-Type test.txt
+New-Item –Path c:\temp\tmp\test1.txt –type file
+Set-Content tes1t.txt “This is a test.”
+Add-Content test1.txt "This is another test."
+Get-Content test1.txt
+Set-Alias Add-Content Copy-Con
+Copy-Con test2.txt "This is a test."
+Copy-Con test2.txt "This is another test."
+Get-Content test2.txt
